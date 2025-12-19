8c7138c878325a7ad81be2a1b58e8ed33d12e47a
vickie25 <nyandorovictor3900@gmail.com>
2025-12-19 20:01:02 +0300

    string updated

--- src/containers/about-me/index.tsx
+++ src/containers/about-me/index.tsx
@@ -248,7 +248,7 @@ const KeySkills = () => {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { type: "spring", stiffness: 300, damping: 20 },
+      transition: { type: "spring" as const, stiffness: 300, damping: 20 },
     },
   }
 
